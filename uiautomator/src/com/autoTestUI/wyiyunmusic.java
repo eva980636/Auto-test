5823841b57813ec0e433627bda9bf0d829d31467
eva980636 <147300786@qq.com>
2016-07-29 15:07:55 +0800

    wyiyun add login function

--- uiautomator/src/com/autoTestUI/wyiyunmusic.java
+++ uiautomator/src/com/autoTestUI/wyiyunmusic.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import android.os.RemoteException;
 
 import com.android.uiautomator.core.UiDevice;
+import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class wyiyunmusic extends UiAutomatorTestCase {
@@ -16,7 +18,30 @@ public class wyiyunmusic extends UiAutomatorTestCase {
 		assertTrue("screen on :can't wakeup", device.isScreenOn());
 
 		String appName = "com.netease.cloudmusic/com.netease.cloudmusic.activity.LoadingActivity";
-
+		otoDisplayRun.execCmd("am start -n " + appName);
+		
+		UiObject denglu=new UiObject(new UiSelector().resourceId("com.netease.cloudmusic:id/loginOrRegister"));
+		denglu.click();
+		sleep(2000);
+		
+		UiObject phone=new UiObject(new UiSelector().resourceId("com.netease.cloudmusic:id/login"));
+		phone.clickAndWaitForNewWindow();
+		
+		UiObject username=new UiObject(new UiSelector().resourceId("com.netease.cloudmusic:id/phoneNumber"));
+		username.click();
+		username.setText("18710140362");
+		sleep(200);
+		
+		UiObject passwd=new UiObject(new UiSelector().resourceId("com.netease.cloudmusic:id/password"));
+		passwd.click();
+		passwd.setText("qwer1234");
+		sleep(200);	
+		
+		UiObject btn=new UiObject(new UiSelector().resourceId("com.netease.cloudmusic:id/login"));
+		btn.click();
+		sleep(2000);	
+				
+		
 		window_lib.windowtest(device, appName);
 		// start testing itself
 		/*
