c8845f44d3cd77bebc0b954533ee55b325ad23ba
eva980636 <147300786@qq.com>
2016-09-06 17:28:47 +0800

    add taijievideo test

--- uiautomator/src/com/autoTestUI/taijievideo.java
+++ uiautomator/src/com/autoTestUI/taijievideo.java
@@ -4,29 +4,19 @@ import java.io.IOException;
 import android.os.RemoteException;
 
 import com.android.uiautomator.core.UiDevice;
-import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
-import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
-public class taobao extends UiAutomatorTestCase {
-	public void testtaobao() throws UiObjectNotFoundException, RemoteException,
+public class taijievideo extends UiAutomatorTestCase {
+	public void testtaijievideo() throws UiObjectNotFoundException, RemoteException,
 			IOException, InterruptedException {
 		UiDevice device = getUiDevice();
 		// wake up screen
 		device.wakeUp();
 		assertTrue("screen on :can't wakeup", device.isScreenOn());
 
-		String appName = "com.taobao.apad/com.taobao.apad.activity.MainActivity";
-		otoDisplayRun.execCmd("am start -n " + appName);
-		
-		boolean dumpFirstStart = new UiObject(new UiSelector().resourceId("com.taobao.taobao:id/title")).exists();
-		
-		if (dumpFirstStart == true) {
-			UiObject cancelButton = new UiObject(new UiSelector().text("取消"));
-			
-			cancelButton.click();
-		}	
+		String appName = "com.togic.livevideo/com.togic.launcher.SplashActivity";
+
 		window_lib.windowtest(device, appName);
 		// start testing itself
 		/*
@@ -36,4 +26,6 @@ public class taobao extends UiAutomatorTestCase {
 		 * e.printStackTrace(); } sleep(500);
 		 */
 	}
+
+	
 }
